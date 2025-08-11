22771f768993700368291f7a1be102bd197bd9d1
Jhonatadev7 <jhonatab689@gmail.com>
2025-08-11 00:13:25 +0000

    Projeto COMCLUIDO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 
-int main() {    
+typedef struct {
+    char cidade[50];       // Nome da cidade
+    int populacao;         // População
+    float area;            // Área
+    float pib;             // PIB
+    int pontos_turisticos; // Número de pontos turísticos
+} Carta;
 
-    char Cidade[50];
-    int Populacao;
-    float Area;
-    float PIB;
-    int PontoTuristico;
+void cadastrarCarta(Carta *carta, int numero) {
+    printf("\nCadastro da carta %d\n", numero);
+    printf("Digite o nome da Cidade: ");
+    scanf(" %49[^\n]", carta->cidade); // lê até 49 chars com espaços
+    printf("Digite a populacao: ");
+    scanf("%d", &carta->populacao);
+    printf("Digite a area: ");
+    scanf("%f", &carta->area);
+    printf("Digite o PIB: ");
+    scanf("%f", &carta->pib);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &carta->pontos_turisticos);
+}
 
-   // Prenchemento de DADOS.
-    printf("Cidade:\n");
-    scanf(" %[^\n]", Cidade);
+void exibirCarta(Carta carta, int numero) {
+    printf("\nCarta %d: %s\n", numero, carta.cidade);
+    printf("Populacao: %d\n", carta.populacao);
+    printf("Area: %.2f\n", carta.area);
+    printf("PIB: %.2f\n", carta.pib);
+    printf("Pontos turisticos: %d\n", carta.pontos_turisticos);
+}
 
-    printf("População:\n");
-    scanf("%d", &Populacao);
+int main() {
+    Carta carta1, carta2;
 
-    printf("Área:\n");
-    scanf("%f", &Area);
+    cadastrarCarta(&carta1, 1);
+    cadastrarCarta(&carta2, 2);
 
-    printf("PIB:\n");
-    scanf("%f", &PIB);
-
-    printf("Ponto Turístico:\n");
-    scanf("%d", &PontoTuristico);
-
-    // Mostrar dados das CARTAS
-    printf("\nCARTAS DE USUARIO #1\n");
-    printf("Cidade: %s\n", Cidade);
-    printf("População: %d\n", Populacao);
-    printf("Área: %.2f\n", Area);
-    printf("PIB: %.2f\n", PIB);
-    printf("Ponto Turístico: %d\n", PontoTuristico);
+    printf("\n--- Dados cadastrados ---\n");
+    exibirCarta(carta1, 1);
+    exibirCarta(carta2, 2);
 
     return 0;
 }
-
